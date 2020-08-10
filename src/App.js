2ea9b9d7f8186c9bdd873a2d1aa47145b39f8b1d
meherafroz <meherafroz6331@gmail.com>
2020-08-10 12:18:50 +0600

    navbar & link

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import React, { Component } from 'react';
-import Todos from './Todos';
-import AddForm from './AddForm';
+import Navbar from './component/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './component/Home'
+import About from './component/About'
+import Contact from './component/Contact'
 
 class App extends Component {
-    state = {
-        todos: [
-            { id: 1, content: 'buy milk' },
-            { id: 2, content: 'play football' },
-        ],
-    };
-
-    deleteTodo = (id) => {
-        const todos = this.state.todos.filter((todo) => {
-            return todo.id !== id;
-        });
-        this.setState({
-            todos,
-        });
-    };
-
-    addTodo = (todo) => {
-        todo.id = Math.random();
-        const newTodo = [...this.state.todos, todo];
-        this.setState({
-            todos: newTodo,
-        });
-    };
-
+    
     render() {
         return (
-            <div className="todo-app container">
-                <h1 className="center blue-text">Todos's</h1>
-                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-                <AddForm addTodo={this.addTodo} />
+          <BrowserRouter>
+            <div className="todo-app">
+                <Navbar/>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/contact' component={Contact} />
             </div>
+          </BrowserRouter>
+            
         );
     }
 }
